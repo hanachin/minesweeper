4dd89155564192a2e64553147dd5272ac41252d0
Seiei Higa <hanachin@gmail.com>
2015-01-30 00:31:25 +0900

    Add cli usage and arguments

--- minesweeper.go
+++ minesweeper.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"./minesweeper"
 )
 
 func main() {
+	bomb := flag.Int("b", 100, "number of bombs")
+	width := flag.Int("w", 40, "width")
+	height := flag.Int("h", 20, "height")
+	flag.Parse()
+
 	g := minesweeper.NewGame()
-	g.SetSquareMap(40, 20, 100)
+	g.SetSquareMap(*width, *height, *bomb)
 	g.Start()
 }
